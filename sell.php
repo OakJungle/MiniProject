06fef23bd684b04c7c6ef4b556ced657698b2204
DeekshithSH <shdeekshith@gmail.com>
2024-02-08 00:07:13 +0530

    Total Price

--- sell.php
+++ sell.php
@@ -20,41 +20,72 @@ if ($result->num_rows <= 0) {
 ?>
 
 <html>
+
 <head>
   <style>
-    body{
+    body {
       background-image: url("https://images.slurrp.com/prodarticles/bmgl61rn2ge.webp?impolicy=slurrp-20210601&width=1200&height=900");
       background-repeat: no-repeat;
       background-size: cover;
     }
-    </style>
+  </style>
   <link rel="stylesheet" href="style.css">
+  <script>
+    function update_total() {
+    var table = document.getElementById("item-menu")
+    var tprice = document.getElementById("total-price")
+    var total = 0
+    var tl = table.rows.length - 2
+      for (var x = 1; x < tl; x = x + 1) {
+        // console.log(table.rows[x].cells[2].getAttribute("price"));
+        var price = table.rows[x].cells[2].getAttribute("price")
+        var qty = table.rows[x].cells[1].firstChild.value
+        if (qty) {
+          total += price * qty;
+        }
+      }
+      tprice.innerHTML = total
+    }
+  function clear_form(){
+    document.getElementById("sell-form").reset()
+    document.getElementById("total-price").innerHTML=0
+  }
+  </script>
 </head>
+
 <body>
-<center >
-<form method="get" action="bill.php">
-<input type="text" value="sell" name="type" style="display: none;">
-<table id="item-menu" class="input">
-  <tr>
-      <td style="">Item Name:</td>
-      <td >Item Quantity:</td> 
-      <td >Price</td>
-  </tr>
-<?php
+  <center>
+    <form id="sell-form" method="get" action="bill.php">
+      <input type="text" value="sell" name="type" style="display: none;">
+      <table id="item-menu" class="input">
+        <tr>
+          <td style="">Item Name:</td>
+          <td>Item Quantity:</td>
+          <td>Price</td>
+        </tr>
+        <?php
 
 while($row = $result->fetch_assoc()) {
   echo "<tr>
   <td>$row[item]</td>
-  <td><input type=number id='quantity' name='quantity[$row[item]]' min=0 max=$row[quantity]></td>
-  <td>$row[price]</td></tr>";
+  <td><input onchange='update_total()' onkeyup='update_total()' type=number id='quantity' name='quantity[$row[item]]' min=0 max=$row[quantity]></td>
+  <td price=$row[price]>$row[price]</td></tr>";
 }
 ?>
-  <tr>
-  <td colspan="2"><input type="reset" value="clear">
-  <input type="button" value="Back" onclick="window.location.href = 'index.html';"></td>
-  <td><input type="submit" value="buy" name="submit"></td>
-  </tr>
-</table>
-</form></center>
+        <tr>
+          <td>Total:</td>
+          <td colspan="2">
+            <p style="text-align: left;" id="total-price">0</p>
+          </td>
+        </tr>
+        <td colspan="2"><input type="reset" onclick="clear_form();" value="clear">
+          <input type="button" value="Back" onclick="window.location.href = 'index.html';">
+        </td>
+        <td><input type="submit" value="buy" name="submit"></td>
+        </tr>
+      </table>
+    </form>
+  </center>
 </body>
+
 </html>
